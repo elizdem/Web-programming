925a5436a33bf926ee054e060d4696469135fce7
elizdem <91390634+elizdem@users.noreply.github.com>
2022-11-16 21:53:28 +0300

    Add lab 4

---
+++ Lab4/pwa.js
@@ -0,0 +1,11 @@
+// Service Worker Register 
+window.addEventListener('load', async () =>{
+    if('serviceWorker' in navigator){
+        try{
+            const reg = await navigator.serviceWorker.register('service-worker.js')
+            console.log('Service worker register success', reg)
+        } catch (e){
+            console.log('Service worker register fail')
+        }
+    }
+})
